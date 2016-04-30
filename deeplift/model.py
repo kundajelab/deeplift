97961bd762e3f41b187951d821b9a593aac17965
Avanti Shrikumar <avanti.shrikumar@gmail.com>
2016-04-30 04:29:27 -0700

    periodic commit
    Former-commit-id: 6d812f8eb37727bdca874f2aafc5659ae6ab0312

--- deeplift/model.py
+++ deeplift/model.py
@@ -13,18 +13,36 @@ if (scripts_dir is None):
                     +" the deeplift directory")
 sys.path.insert(0, scripts_dir)
 import deeplift_util
+from blobs import ScoringMode
+import deeplift_backend as B
 
 
-ScoringMode = deeplift_util.enum(OneAndZeros="OneAndZeros")
+FuncType = deeplift_util.enum(contribs="contribs", multipliers="multipliers")
 
 
 class Model(object):
     
-    def get_contrib_func(input_layer, target_layer, task_idx, scoring_mode):
-        raise NotImplementedError()
+    def _get_func(input_layer,
+                         target_layer,
+                         task_idx,
+                         func_type):
+        target_layer.update_task_index(task_idx)
+        if (func_type == FuncType.contribs):
+            output_symbolic_vars = input_layer.get_target_contrib_vars()
+        elif (func_type == FuncType.multipliers):
+            output_symbolic_vars = input_layer.get_mxts()
+        else:
+            raise RuntimeError("Unsupported func_type: "+func_type)
+        return B.function([input_layer.get_activation_vars()],
+                          output_symbolic_vars)
 
-    def get_contrib_to_pre_activation_func(
-        input_layer, target_layer, task_idx):
+    def get_target_contribs_func(*args, **kwargs):
+        return self._get_func(*args, **kwargs, func_type=FuncType.contribs)
+
+    def get_multipliers_func(*args, **kwargs):
+        return self._get_func(*args, **kwargs, func_type=FuncType.multipliers)
+
+    def set_target_layer_pre_activation(target_layer):
 
         assert len(target_layer.get_output_layers())==0
         final_activation_layer = target_layer.get_output_layers()[0]
@@ -36,22 +54,21 @@ class Model(object):
         if (final_activation_type == "Sigmoid"):
             scoring_mode=ScoringMode.OneAndZeros
         elif (final_activation_type == "Softmax"):
-            raise NotImplementedError()
+            scoring_mode=ScoringMode.SoftmaxPreActivation
         else:
             raise RuntimeError("Unsupported final_activation_type: "
                                +final_activation_type)
 
-        return self.get_contrib_func(
-                    input_layer=input_layer,
-                    target_layer=target_layer,
-                    task_idx=task_idx,
-                    scoring_mode=scoring_mode)    
+        target_layer.set_scoring_mode(scoring_mode)    
         
 
 class SequentialModel(Model):
     
     def __init__(self, layers):
         self._layers = layers
+
+    def set_target_layer_pre_activation(self):
+        self.set_target_layer_pre_activation(self._layers[-2])
 
     def get_layers(self):
         return self._layers
