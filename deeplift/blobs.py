9cc62fbf810b6709def3d47084201443acc36cb7
Avanti Shrikumar <avanti.shrikumar@gmail.com>
2016-05-15 20:18:34 -0700

    maxout in progress

--- deeplift/blobs.py
+++ deeplift/blobs.py
@@ -560,6 +560,187 @@ class ZeroPad2D(SingleInputMixin, Node):
         pass #not used
 
 
+class Maxout(SingleInputMixin, Node):
+
+    def __init__(self, W, b, **kwargs):
+        """
+            W: has dimensions: nb_features, input_dim, output_dim
+            b: has dimensions: nb_features, output_dim
+        """
+        super(Maxout, self).__init__(**kwargs)
+        self.W = W
+        if (b is None):
+            b = np.zeros((self.W.shape[0], self.W.shape[2]))
+        self.b = b
+        #precompute the difference between the weight vectors for
+        #each feature. W_differences will have dimensions:
+        #[feature being subtracted from][feature being subtracted]
+        #[input][output]
+        self.W_differences = np.zeros((self.W.shape[0], self.W.shape[0],
+                                       self.W.shape[1], self.W.shape[2]))
+        self.b_differences = np.zeros((self.b.shape[0], self.b.shape[0],
+                                      self.b.shape[1])) 
+        for feature_idx, feature_vectors in enumerate(self.W):
+            self.W_differences[feature_idx] =\
+                self.W[feature_idx][None,:,:] - self.W
+            self.b_differences[feature_idx] =\
+                self.b[feature_idx,:][None,:] - self.b
+         
+
+    def _build_activation_vars(self, input_act_vars):
+        #self.W has dims: num_features x num_inputs x num_outputs
+        #input_act_vars has dims: batch x num_inputs
+        #separate_feature_activations therefore has dims:
+        # batch x num_features x num_outputs
+        separate_feature_activations = B.dot(input_act_vars, self.W)
+        separate_feature_activations += self.b
+        self._separate_feature_activations = separate_feature_activations
+        
+        #_max_activations has dims: batch x num_outputs
+        self._max_activations =\
+            B.max(separate_feature_activations, axis=1)
+        return self._max_activations 
+
+    def _get_actual_active_gradients():
+        #get the gradients ("features") that were active for each
+        #batch x output combination at actual input value
+        #self._max_activations has dims: batch x num_outputs
+        #separate_feature_activations has dims:
+        # batch x num_features x num_outputs
+        #active_gradients_mask will have dims:
+        # batch x num_features x num_outputs
+        active_gradients_mask = 1.0*(self._separate_feature_activations==\
+                                 self._max_activations[:,None:])
+        #divide by the sum in case of ties
+        active_gradients_mask = active_gradients_mask/\
+                           B.sum(active_gradients_mask, axis=1)
+
+        #active_gradients_mask has dims:
+        # batch x num_features x num_outputs
+        #self.W has dims: num_features x num_inputs x num_outputs
+        #active_gradients will have dims: batch x num_inputs x num_outputs
+        active_gradients = B.sum(self.W[None,:,:,:]*\
+                                 (active_gradients_mask[:, :, None, :]),
+                                 axis=1)
+        return active_gradients
+
+    def _get_weighted_active_gradients():
+        #get gradients ("features") weighted by how much they
+        #'dominate' on a vector
+        #from the default value of the input to the actual input value
+        inp_diff_from_default = self._get_input_diff_from_default_vars()
+        #compute differences in each feature activation at default
+        #_get_input_default_activation_vars() has dims: batch x num_inputs
+        #W_differences has dims:
+        # num_features x num_features x num_inputs x num_outputs
+        #b_differences has dims:
+        # num_features x num_features x num_outputs
+        #feature_diff_at_default therefore has dims:
+        # batch x num_features x num_features x num_outputs
+        feature_diff_at_default =\
+            B.dot(self._get_input_default_activation_vars()
+                  , self.W_differences)\
+            + self.b_differences
+
+        #dot product with unit vector in the difference-from-default dir
+        #change_vec_projection has dim batch x 
+        #inp_diff_from_default has dims: batch x num_inputs 
+        #W_differences has dims:
+        # num_features x num_features x num_inputs x num_outputs
+        #change_vec_projection therefore has dims:
+        # batch x num_features x num_features x num_outputs
+        change_vec_projection =\
+            B.dot(inp_diff_from_default, self.W_differences) 
+
+        #add a pseudocount to all the positions that are near zero
+        #so division is not sad
+        change_vec_projection += NEAR_ZERO_THRESHOLD\
+                                 *(B.abs(change_vec_projection)\
+                                   < NEAR_ZERO_THRESHOLD)
+        positive_change_vec_mask = change_vec_projection > 0
+        negative_change_vec_mask = change_vec_projection < 0
+
+        #find the theta that indicates how far along the diff-from-default 
+        #vector the planes of the features intersect
+        #'thetas' has dimensions:
+        # batch x num_features x num_features x num_outputs
+        thetas = feature_diff_at_default/change_vec_projection
+        
+        #matrix of thetas for transitioning in or transitioning out
+        #when two features are exactly equal, will set the values
+        #for transition_in_thetas or transition_out_thetas to be either
+        #+inf or -inf, with lower indices dominating over higher indices
+        #these all have dimensions num_features x num_features
+        upper_triangular_inf = np.triu(np.inf*
+                                np.ones((self.W.shape[0],
+                                         self.W.shape[0])))
+        lower_triangular_inf = np.tril(np.inf*(
+                                np.ones((self.W.shape[0], self.W.shape[0]))
+                                -np.eye(self.W.shape[0])))
+        transition_in_equality_vals = -1*(upper_triangular_inf)\
+                                      + lower_triangular_inf
+        transition_out_equality_vals= -1*transition_in_equality_mask
+
+        #equal pairs masks
+        #W_differences has dims:
+        # num_features x num_features x num_inputs x num_outputs
+        #equal_pairs_mask and unequal_pairs_mask therefore have dims:
+        # num_features x num_features x num_outputs
+        equal_pairs_mask = (T.sum(T.abs(self.W_differences), axis=2)==0)*1
+        unequal_pairs_mask = 1-equal_pairs_mask 
+
+        #the pos/neg change_vec masks have dimensions:
+        # batch x num_features x num_features x num_outputs
+        #thetas have dimensions:
+        # batch x num_features x num_features x num_outputs
+        # eq/uneq pairs masks have dims:
+        # num_features x num_features x num_outputs
+        # transition in/out equality vals have dims:
+        # num_features x num_features
+        #transition_in/out_thetas therefore has dims:
+        # batch x num_features x num_features x num_outputs
+        transition_in_thetas =\
+         ((equal_pairs_mask\
+           *transition_in_equality_vals[:,:,None])[None,:,:,:]) + 
+         (unequal_pairs_mask[None,:,:,:]\
+             *(positive_change_vec_mask*thetas))
+        transition_out_thetas =\
+         ((equal_pairs_mask\
+           *transition_out_equality_vals[:,:,None])[None,:,:,:]) + 
+         (unequal_pairs_mask[None,:,:,:]\
+             *(negative_change_vec_mask*thetas))
+
+        #time_spent_per_feature has dims:
+        # batch x num_features x num_outputs 
+        time_spent_per_feature = B.maximum(0,
+             B.minimum(1, B.min(transition_out_thetas, axis=2))
+             - B.maximum(0, B.max(transition_in_thetas, axis=2))) 
+
+        #time_spent_per_feature has dims:
+        # batch x num_features x num_outputs
+        #self.W has dims: num_features x num_inputs x num_outputs
+        #weighted ws therefore has dims: batch x num_inputs x num_outputs
+        weighted_ws = B.sum(
+                      time_spent_per_feature[:,:,None,:]\
+                      *self.W[None,:,:,:], axis=2)
+
+    def _get_mxts_increments_for_inputs(self):
+        #get the vector for the diff from default
+        #inp_diff_from_default has dims:
+        # batch x num_inputs
+        inp_diff_from_default = self._get_input_diff_from_default_vars()
+        sum_squares_diff_from_default =\
+         B.sum(B.square(inp_diff_from_default), axis=1)
+        
+
+        else:
+            return B.dot(self.get_mxts(),weighted_ws.T)
+             
+
+    def _build_gradient_at_default_activation(self):
+        pass #not used
+    
+
 class RNN(SingleInputMixin, Node):                                              
                                                                                 
    def __init__(self, weights=None, expose_all_hidden=False,                    
