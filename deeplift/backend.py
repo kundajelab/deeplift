8efb68e11327956c4aedbb8e72a9e7452e54eb5f
Avanti Shrikumar <avanti.shrikumar@gmail.com>
2016-06-27 09:22:20 -0700

    reorganized files to be compatible with package import
    Former-commit-id: a09197053844f18a78c7fdc5846cd621bdf680e3

---
+++ deeplift/backend.py
@@ -0,0 +1,227 @@
+import sys, os
+import theano
+from theano import tensor as T
+scripts_dir = os.environ.get("DEEPLIFT_DIR")
+if (scripts_dir is None):
+    raise Exception("Please set environment variable DEEPLIFT_DIR to point to"
+                    +" the deeplift directory")
+sys.path.insert(0, scripts_dir)
+import deeplift.util 
+
+PoolMode = deeplift.util.enum(max='max', avg='avg')
+BorderMode = deeplift.util.enum(same='same', half='half', valid='valid')
+
+
+def reshape(tensor, shape):
+    return T.reshape(tensor, shape)
+
+
+def pow(inp, power):
+    return T.pow(inp, power)
+
+
+def maximum(x, y):
+    return T.maximum(x, y)
+
+
+def minimum(x, y):
+    return T.minimum(x, y)
+
+
+def as_tensor_variable(x, name, ndim):
+   return T.as_tensor_variable(x, name=name, ndim=ndim) 
+
+
+def max(x, axis):
+    return T.max(x, axis=axis)
+
+
+def min(x, axis):
+    return T.min(x, axis=axis)
+
+
+def argmax(x, axis):
+    return T.argmax(x, axis=axis)
+
+
+def square(x):
+    return T.sqr(x)
+
+
+def sqrt(x):
+    return T.sqrt(x) 
+
+
+def sum(x, axis):
+    return T.sum(x, axis=axis)
+
+
+def ones_like(x):
+    return T.ones_like(x)
+
+
+def zeros_like(x):
+    return T.zeros_like(x)
+
+
+def set_subtensor(subtensor, amnt):
+    return T.inc_subtensor(subtensor, amnt, set_instead_of_inc=True)
+
+
+def function(inputs, outputs, **kwargs):
+    return theano.function(inputs, outputs,
+                            allow_input_downcast=True, **kwargs) 
+
+
+def tensor_with_dims(num_dims, name):
+    return T.TensorType(dtype=theano.config.floatX,
+                        broadcastable=[False]*num_dims)(name)
+
+
+def shared(value):
+    return theano.shared(value=value)
+
+
+def dot(x, y):
+    return T.dot(x, y)
+
+
+def relu(inp):
+    return T.nnet.relu(inp)
+
+
+def sigmoid(inp):
+    return T.nnet.sigmoid(inp)
+
+
+def softmax(inp):
+    return T.nnet.softmax(inp)
+
+
+def sigmoid_grad(inp):
+    out = sigmoid(inp)
+    grad = T.nnet.sigmoid.grad((inp,), (out,))
+    return grad
+
+
+def softmax_grad(inp):
+    out = softmax(inp)
+    grad = T.nnet.Softmax().grad((inp,), (out,))
+    return grad
+
+
+def abs(inp):
+   return T.abs_(inp) 
+
+
+def conv2d(inp, filters, border_mode, subsample):
+    
+    if (border_mode==BorderMode.same):
+        #'half' kernel width padding results in outputs of the same
+        #dimensions as input
+        border_mode=BorderMode.half
+        assert filters.shape[2]%2 == 1 and filter_shape[3]%2 == 1,\
+            "haven't handled even filter shapes for border mode 'half'"
+    return T.nnet.conv2d(input=inp,
+                         filters=theano.shared(value=filters),
+                         border_mode=border_mode,
+                         subsample=subsample,
+                         filter_shape=filters.shape)
+
+
+def conv2d_grad(out_grad, conv_in, filters, border_mode, subsample):
+    conv_op = T.nnet.conv.ConvOp(output_mode=border_mode,
+                                 dx=subsample[0],
+                                 dy=subsample[1]) 
+    inverse_conv2d = conv_op.grad(
+                       (conv_in,
+                       T.as_tensor_variable(filters)),
+                       (out_grad,))
+    #grad returns d_input and d_filters; we just care about
+    #the first
+    return inverse_conv2d[0]
+
+
+def get_pooling_padding_and_theano_pool_mode(
+    pool_size, border_mode, pool_mode):
+    if border_mode == BorderMode.same:
+        padding = [x - (2 if x%2==1 else 1) for x in pool_size] 
+    elif border_mode == BorderMode.valid:
+        padding = (0, 0) 
+    else:
+        raise RuntimeError("Valid border modes are: "+str(BorderMode.vals)
+                           +", got: "+str(border_mode))
+
+    if (pool_mode == PoolMode.max):
+        theano_pool_mode = 'max'
+    elif (pool_mode == PoolMode.avg):
+        theano_pool_mode = 'average_exc_pad'
+    else:
+        raise RuntimeError("Valid pool modes are: "+str(PoolMode.vals)
+                           +", got: "+str(pool_mode))
+    return padding, theano_pool_mode
+
+def pool2d(inp, pool_size, strides, border_mode, ignore_border, pool_mode):
+
+    padding, theano_pool_mode = get_pooling_padding_and_theano_pool_mode(
+                                    pool_size, border_mode, pool_mode)
+
+    to_return = T.signal.pool.pool_2d(input=inp,
+                    ds=pool_size,
+                    ignore_border=ignore_border,
+                    st=strides,
+                    padding=padding,
+                    mode=pool_mode)
+
+    if border_mode==BorderMode.same:
+        final_shape = [(inp.shape[2+i] + strides[i] - 1)//strides[i]
+                       for i in [0,1]]
+        to_return = to_return[:, :, final_shape[0], final_shape[1]]
+    
+    return to_return
+
+
+def pool2d_grad(pool_out, pool_in,
+                   pool_size, strides, border_mode,
+                   ignore_border, pool_mode):
+    if (border_mode == BorderMode.same):
+        raise RuntimeError("I have not handled the cropping for border mode"
+                           "'same' yet")
+    padding, theano_pool_mode = get_pooling_padding_and_theano_pool_mode(
+                                    pool_size, border_mode, pool_mode)
+    pool_op = T.signal.pool.Pool(ds=pool_size,
+                                 st=strides,
+                                 ignore_border=ignore_border,
+                                 padding=padding,
+                                 mode=pool_mode)
+    return pool_op.grad((pool_in,),
+                       (pool_out,))[0]
+    
+
+def flatten_keeping_first(x):
+    """
+        Flatten all but the first dimension
+    """
+    return T.reshape(x, (x.shape[0], T.prod(x.shape[1:]))) 
+
+
+def unflatten_keeping_first(x, like):
+    """
+        shape x to resemble the shape of 'like'
+    """
+    return T.reshape(x, like.shape)
+
+
+def zeropad2d(x, padding):
+    output = T.zeros((x.shape[0], #batch
+                      x.shape[1], #channel
+                      x.shape[2] + 2*padding[0], #rows
+                      x.shape[3] + 2*padding[1])) #cols
+    output = set_subtensor(output[:, :, padding[0]:x.shape[2]+padding[0],
+                                        padding[1]:x.shape[3]+padding[1]], x)
+    return output 
+
+
+def discard_pad2d(x, padding):
+    return x[:, :, padding[0]:(x.shape[2]-padding[0]),
+                   padding[1]:(x.shape[3]-padding[0])]
---
+++ deeplift/util.py
@@ -0,0 +1,137 @@
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+import sys
+import os
+import numpy as np
+from collections import namedtuple
+from collections import OrderedDict
+
+def enum(**enums):
+    class Enum(object):
+        pass
+    to_return = Enum;
+    for key,val in enums.items():
+        if hasattr(val, '__call__'): 
+            setattr(to_return, key, staticmethod(val))
+        else:
+            setattr(to_return,key,val);
+    to_return.vals = [x for x in enums.values()];
+    to_return.the_dict = enums
+    return to_return;
+
+
+def assert_is_type(instance, the_class, instance_var_name):
+    if (not superclass_in_base_classes(
+             instance.__class__.__bases__, the_class)):
+        raise RuntimeError(instance_var_name+" should be an instance of "
+                +the_class.__name__+" but is "+str(instance.__class__)); 
+    return True
+
+
+def superclass_in_base_classes(base_classes, the_class):
+    """
+        recursively determine if the_class is among or is a superclass of
+         one of the classes in base_classes. The comparison is done by
+         name so that even if reload is called on a module, this still
+         works.
+    """
+    for base_class in base_classes:
+        if base_class.__name__ == the_class.__name__:
+            return True 
+        else:
+            #if the base class in turn has base classes of its own
+            if len(base_class.__bases__)!=1 or\
+                base_class.__bases__[0].__name__ != 'object':
+                #check them. If there's a hit, return True
+                if (superclass_in_base_classes(
+                    base_classes=base_class.__bases__,
+                    the_class=the_class)):
+                    return True
+    #if 'True' was not returned in the code above, that means we don't
+    #have a superclass
+    return False
+
+
+def run_function_in_batches(func,
+                            input_data_list,
+                            batch_size=10,
+                            progress_update=1000):
+    #func has a return value such that the first index is the
+    #batch. This function will run func in batches on the inputData
+    #and will extend the result into one big list.
+    assert isinstance(input_data_list, list), "input_data_list must be a list"
+    #input_datas is an array of the different input_data modes.
+    to_return = [];
+    i = 0;
+    while i < len(input_data_list[0]):
+        if (progress_update is not None):
+            if (i%progress_update == 0):
+                print("Done",i)
+        to_return.extend(func(*[x[i:i+batch_size] for x in input_data_list]));
+        i += batch_size;
+    return to_return
+
+def mean_normalise_weights_for_sequence_convolution(weights,
+                                                    bias,
+                                                    normalise_across_rows,
+                                                    weightsHeight=4):
+    #weights: outputchannels, inputChannels, windowDims
+    assert len(weights.shape)==4
+    assert weights.shape[1]==1, weights.shape
+    axis_for_normalisation = 2 if normalise_across_rows else 3
+    if (normalise_across_rows):
+        assert weights.shape[axis_for_normalisation]==\
+         weightsHeight, weights.shape
+    else:
+        assert weights.shape[axis_for_normalisation]==\
+         weightsHeight, weights.shape
+        
+    mean_weights_at_positions=np.mean(weights,axis=axis_for_normalisation)
+    new_bias = bias + np.sum(np.sum(mean_weights_at_positions,
+                                    axis=2),axis=1)
+    if (normalise_across_rows):
+        mean_weights_at_positions=mean_weights_at_positions[:,:,None,:]
+    else:
+        mean_weights_at_positions=mean_weights_at_positions[:,:,:,None]
+    renormalised_weights=weights-mean_weights_at_positions
+    return renormalised_weights, new_bias
+
+
+def get_mean_normalised_softmax_weights(weights, biases):
+    new_weights = weights - np.mean(weights, axis=1)[:,None]
+    new_biases = biases - np.mean(biases)
+    return new_weights, new_biases
+
+def get_effective_width_and_stride(widths,strides):
+    effectiveStride = strides[0] 
+    effectiveWidth = widths[0]
+    assert len(strides)==len(widths)
+    if len(strides)>1:
+        for (stride, width) in zip(strides[1:],widths[1:]):
+            effectiveWidth = ((width-1)*effectiveStride)+effectiveWidth 
+            effectiveStride = effectiveStride*stride
+    return effectiveWidth, effectiveStride
+
+def get_lengthwise_widths_and_strides(layers):
+    """
+        layers: a list of convolutional/pooling blobs
+    """
+    import deeplift.blobs
+    widths = [] 
+    strides = []
+    for layer in layers:
+        if type(layer).__name__ == "Conv2D":
+            strides.append(layer.strides[1]) 
+            widths.append(layer.W.shape[3])
+        elif isinstance(layer, deeplift.blobs.Activation):
+            pass
+        else:
+            raise RuntimeError("Please implement how to extract width and"
+                               "stride from layer of type: "
+                               +type(layer).__name__)
+    return widths, strides
+
+def get_lengthwise_effective_width_and_stride(layers):
+    widths, strides = get_lengthwise_widths_and_strides(layers)
+    return get_effective_width_and_stride(widths, strides)
