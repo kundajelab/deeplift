2ed038764a600128f4c064934c285787eeb99917
Avanti Shrikumar <avanti.shrikumar@gmail.com>
2016-09-19 20:27:02 -0700

    deleted extraneous examples
    Former-commit-id: b833a4171b59c3304408c2ab2d40c9e1dd52a552 [formerly df8a5071720282ca76d8b08d70740a5989bf9673]
    Former-commit-id: 2e15cc60638107bdaf4122d3c38745defd12ced4

--- examples/rnn/ctcf_density_gru/prepLabelsAndSplits.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-echo $'id\tlabel' > labels.txt
-zcat *pos*.gz | perl -lane 'if ($. > 1) {print("$F[0]\t1")}' >> labels.txt
-zcat *neg*.gz | perl -lane 'if ($. > 1) {print("$F[0]\t0")}' >> labels.txt
-mkdir splits
-cat labels.txt | perl -lane 'if (($.%10 != 1) && ($.%10 != 2)) {print $_}' > splits/train.txt
-cat labels.txt | perl -lane 'if ($.%10==1) {print $_}' > splits/valid.txt
-cat labels.txt | perl -lane 'if ($.%10==2) {print $_}' > splits/test.txt
