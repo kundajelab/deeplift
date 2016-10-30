81aac5178ee029a7b2350fdc88b71f52bacee5fb
Avanti Shrikumar <avanti.shrikumar@gmail.com>
2016-10-30 18:46:33 -0400

    Now the reference is an additional input argument
    Former-commit-id: 3efc00e68de4166d02a40d46e89e75ce101b636a [formerly 3034f9e4a00c7af88af2d85a92ef79345df0dc49]
    Former-commit-id: 73e8a5bf0555e376602071fbb792e0cf81218bd0

--- tests/blobs/test_dense.py
+++ tests/blobs/test_dense.py
@@ -7,6 +7,7 @@ import sys
 import os
 import numpy as np
 import deeplift.blobs as blobs
+from deeplift import backend as B
 from deeplift.blobs import DenseMxtsMode
 import theano
 
@@ -14,10 +15,8 @@ import theano
 class TestDense(unittest.TestCase):
 
     def setUp(self):
-        self.input_layer = blobs.Input_FixedReference(
-                            reference=0.0,
-                            num_dims=None,
-                            shape=(None,4))
+        self.input_layer = blobs.Input(num_dims=None,
+                                       shape=(None,4))
         self.w1 = [1.0, 2.0, 3.0, 4.0]
         self.w2 = [-1.0, -2.0, -3.0, -4.0]
         W = np.array([self.w1, self.w2]).T
@@ -33,20 +32,22 @@ class TestDense(unittest.TestCase):
                     [2.0, 2.0, 2.0, 2.0]]
         
     def test_dense_fprop(self): 
-        func = theano.function([self.input_layer.get_activation_vars()],
-                                self.dense_layer.get_activation_vars(),
-                                allow_input_downcast=True)
+        func = B.function([self.input_layer.get_activation_vars()],
+                           self.dense_layer.get_activation_vars())
         self.assertListEqual([list(x) for x in func(self.inp)],
                              [[9.0,-9.0], [19.0, -19.0]])
 
+    @skip
     def test_dense_backprop(self):
-        func = theano.function([self.input_layer.get_activation_vars()],
-                                self.input_layer.get_mxts(),
-                                allow_input_downcast=True)
+        func = B.function([self.input_layer.get_activation_vars(),
+                           self.input_layer._get_default_activation_vars()],
+                           self.input_layer.get_mxts())
         self.dense_layer.update_task_index(task_index=0)
-        self.assertListEqual([list(x) for x in func(self.inp)],
+        self.assertListEqual([list(x) for x in func(
+                              self.inp, np.zeros_like(self.inp))],
                              [self.w1, self.w1])
         self.dense_layer.update_task_index(task_index=1)
-        self.assertListEqual([list(x) for x in func(self.inp)],
+        self.assertListEqual([list(x) for x in func(self.inp,
+                              np.zeros_like(self.inp))],
                              [self.w2, self.w2])
         
