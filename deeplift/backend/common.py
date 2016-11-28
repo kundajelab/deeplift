92dad6b5b67232cb0762935d72b53fb388d083f4
Avanti Shrikumar <avanti.shrikumar@gmail.com>
2016-11-28 14:47:28 -0800

    wrote more tensorflow code
    Former-commit-id: 6d2f463362a4c909c80171d834b8d7e3ac692617 [formerly 94e6c13813dd00267598e6d078e699656fa179e4]
    Former-commit-id: 1d290e5a6d5c7883ce06d417b24d84eba5ab8d9e

--- deeplift/backend/common.py
+++ deeplift/backend/common.py
@@ -1,9 +1,5 @@
 import deeplift.util
 from deeplift.util import NEAR_ZERO_THRESHOLD
-
-PoolMode = deeplift.util.enum(max='max', avg='avg')
-BorderMode = deeplift.util.enum(same='same', half='half', valid='valid')
-
 from . import theano_backend as B
 
 def apply_iteratively_to_list(the_list, function_to_apply):
--- deeplift/blobs/convolution.py
+++ deeplift/blobs/convolution.py
@@ -3,6 +3,9 @@ from __future__ import print_function
 from __future__ import absolute_import
 from .core import *
 
+PoolMode = deeplift.util.enum(max='max', avg='avg')
+PaddingMode = deeplift.util.enum(SAME='SAME', VALID='VALID')
+
 
 class Conv2D(SingleInputMixin, Node):
     """
@@ -10,10 +13,10 @@ class Conv2D(SingleInputMixin, Node):
             the weights are 'flipped'
     """
 
-    def __init__(self, W, b, strides, border_mode, **kwargs):
+    def __init__(self, W, b, strides, padding_mode, **kwargs):
         """
             The ordering of the dimensions is assumed to be:
-                channels, rows, columns (i.e. theano consistent)
+                rows, columns, channels
             Note: this is ACTUALLY a convolution and not a cross-correlation,
                 i.e. the weights are 'flipped' and then you do cross-corr.
                 This is the behaviour that keras has, but not all deep
@@ -21,22 +24,19 @@ class Conv2D(SingleInputMixin, Node):
         """
         super(Conv2D, self).__init__(**kwargs)
         #W has dimensions:
-        #num_output_channels x num_inp_channels
-        #                    x rows_kern_width x cols_kern_width
-        #FIXME: Assumes theano dim order!
+        #rows_kern_width x cols_kern_width x inp_channels x num output channels
         self.W = W
         self.b = b
         self.strides = strides
-        self.border_mode = border_mode
+        self.padding_mode = padding_mode
 
     def set_filter_references(self, filter_reference_activations,
                                     filter_input_references): 
         #filter_references is vec of length num_output_channels;
         #indicates the reference activations 
         #filter_input_references should have same dimensions as W
-        #FIXME: Assumes theano dim order!
-        self.learned_reference = (B.ones_like(self.get_activation_vars())
-                              *filter_reference_activations[None,:,None,None])
+        self.learned_reference = (tf.ones_like(self.get_activation_vars())
+                              *filter_reference_activations[None,None,None,:])
         self.filter_input_references = filter_input_references 
 
     def set_filter_silencing(self, filter_diff_from_ref_silencer):
@@ -55,29 +55,31 @@ class Conv2D(SingleInputMixin, Node):
 
     def _compute_shape(self, input_shape):
         #assuming a theano dimension ordering here...
-        shape_to_return = [None, self.W.shape[0]]
+        shape_to_return = [None]
         if (input_shape is None):
             shape_to_return += [None, None]
         else:
-            if (self.border_mode != B.BorderMode.valid):
+            if (self.border_mode != BorderMode.valid):
                 raise RuntimeError("Please implement shape inference for"
                                    " border mode: "+str(self.border_mode))
             for (dim_inp_len, dim_kern_width, dim_stride) in\
-                zip(input_shape[2:], self.W.shape[2:], self.strides):
+                zip(input_shape[1:3], self.W.shape[:2], self.strides):
                 #assuming that overhangs are excluded
                 shape_to_return.append(
                  1+int((dim_inp_len-dim_kern_width)/dim_stride)) 
+        shape_to_return.append(self.W.shape[-1]) #num output channels
         return shape_to_return
 
     def _build_activation_vars(self, input_act_vars):
         conv_without_bias = self._compute_conv_without_bias(input_act_vars)
-        return conv_without_bias + self.b[None,:,None,None]
+        return conv_without_bias + self.b[None,None,None,:]
 
     def _compute_conv_without_bias(self, x):
-        conv_without_bias =  B.conv2d(inp=x,
-                                  filters=self.W,
-                                  border_mode=self.border_mode,
-                                  subsample=self.strides)
+        conv_without_bias = tf.nn.conv2d(
+                             input=x,
+                             filter=self.W,
+                             strides=(1,)+self.strides+(1,),
+                             padding=self.padding_mode)
         return conv_without_bias
 
     def get_contribs_of_inputs_with_filter_refs(self):
@@ -90,70 +92,43 @@ class Conv2D(SingleInputMixin, Node):
             effective_mxts = self.get_mxts()*silencer_mask
 
         #efficiently compute the contributions of the layer below
-        mult_times_input_on_layer_below = B.conv2d_grad(
-                out_grad=effective_mxts,
-                conv_in=self._get_input_activation_vars(),
-                filters=self.W,
-                border_mode=self.border_mode,
-                subsample=self.strides)*self._get_input_activation_vars()
-        mult_times_filter_ref_on_layer_below = B.conv2d_grad(
-                out_grad=effective_mxts,
-                conv_in=self._get_input_activation_vars(),
+        mult_times_input_on_layer_below = tf.nn.conv2d_transpose(
+                value=effective_mxts,
+                filter=self.W,
+                output_shape=self.get_shape(),
+                strides=(1,)+self.strides+(1,)
+                padding=self.padding_mode)*self._get_input_activation_vars()
+        mult_times_filter_ref_on_layer_below = tf.nn.conv2d_transpose(
+                value=effective_mxts,
                 #reverse the rows and cols of filter_input_references
                 #so that weights line up with the actual position they
                 #act on (remember, convolutions flip things)
-                #FIXME: assumes theano dimension ordering!
-                filters=self.W*self.filter_input_references[:,:,::-1,::-1],
-                border_mode=self.border_mode,
-                subsample=self.strides)
+                filter=self.W*self.filter_input_references[::-1,::-1,:,:],
+                padding=self.padding_mode,
+                strides=(1,)+self.strides+(1,))
         return (mult_times_input_on_layer_below
                 - mult_times_filter_ref_on_layer_below)
          
 
     def _get_mxts_increments_for_inputs(self): 
-        return B.conv2d_grad(
-                out_grad=self.get_mxts(),
-                conv_in=self._get_input_activation_vars(),
-                filters=self.W,
-                border_mode=self.border_mode,
-                subsample=self.strides)
-
-
-class ZeroPad2D(SingleInputMixin, Node):
-
-    def __init__(self, padding, **kwargs):
-        super(ZeroPad2D, self).__init__(**kwargs) 
-        self.padding = padding
-
-    def get_yaml_compatible_object_kwargs(self):
-        kwargs_dict = super(ZeroPad2D, self).\
-                       get_yaml_compatible_object_kwargs()
-        kwargs_dict['padding'] = self.padding
-        return kwargs_dict
-
-    def _compute_shape(self, input_shape):
-        shape_to_return = [None, input_shape[0]] #channel axis the same
-        for dim_inp_len, dim_pad in zip(input_shape[2:], self.padding):
-            shape_to_return.append(dim_inp_len + 2*dim_pad) 
-        return shape_to_return
-
-    def _build_activation_vars(self, input_act_vars):
-        return B.zeropad2d(input_act_vars, padding=self.padding) 
-
-    def _get_mxts_increments_for_inputs(self):
-        return B.discard_pad2d(self.get_mxts(), padding=self.padding)
+        return tf.nn.conv2d_transpose(
+                value=self.get_mxts(),
+                filter=self.W,
+                padding=self.padding_mode,
+                strides=(1,)+self.strides+(1,))
+#TODO:
+#port ZeroPad2D over from theano
 
 
 class Pool2D(SingleInputMixin, Node):
 
-    def __init__(self, pool_size, strides, border_mode,
-                 ignore_border, pool_mode, **kwargs):
+    def __init__(self, pool_size, strides, padding_mode,
+                 ignore_border, **kwargs):
         super(Pool2D, self).__init__(**kwargs) 
         self.pool_size = pool_size 
         self.strides = strides
-        self.border_mode = border_mode
+        self.padding_mode = padding_mode
         self.ignore_border = ignore_border
-        self.pool_mode = pool_mode
 
     def get_yaml_compatible_object_kwargs(self):
         kwargs_dict = super(Pool2D, self).\
@@ -162,28 +137,20 @@ class Pool2D(SingleInputMixin, Node):
         kwargs_dict['strides'] = self.strides
         kwargs_dict['border_mode'] = self.border_mode
         kwargs_dict['ignore_border'] = self.ignore_border
-        kwargs_dict['pool_mode'] = self.pool_mode
         return kwargs_dict
 
     def _compute_shape(self, input_shape):
-        shape_to_return = [None, input_shape[1]] #num channels unchanged 
-        if (self.border_mode != B.BorderMode.valid):
+        shape_to_return = [None] #num channels unchanged 
+        if (self.padding_mode != PaddingMode.valid):
             raise RuntimeError("Please implement shape inference for"
-                               " border mode: "+str(self.border_mode))
+                               " padding mode: "+str(self.padding_mode))
         for (dim_inp_len, dim_kern_width, dim_stride) in\
-            zip(input_shape[2:], self.pool_size, self.strides):
+            zip(input_shape[1:3], self.pool_size, self.strides):
             #assuming that overhangs are excluded
             shape_to_return.append(
              1+int((dim_inp_len-dim_kern_width)/dim_stride)) 
+        shape_to_return.append(input_shape[-1])
         return shape_to_return
-
-    def _build_activation_vars(self, input_act_vars):
-        return B.pool2d(input_act_vars, 
-                      pool_size=self.pool_size,
-                      strides=self.strides,
-                      border_mode=self.border_mode,
-                      ignore_border=self.ignore_border,
-                      pool_mode=self.pool_mode)
 
     def _get_mxts_increments_for_inputs(self):
         raise NotImplementedError()
@@ -216,21 +183,24 @@ class MaxPool2D(Pool2D):
     """ 
     def __init__(self, maxpool_deeplift_mode,
                        **kwargs):
-        super(MaxPool2D, self).__init__(pool_mode=B.PoolMode.max, **kwargs) 
+        super(MaxPool2D, self).__init__(**kwargs) 
         self.maxpool_deeplift_mode = maxpool_deeplift_mode
 
+    def _build_activation_vars(self, input_act_vars):
+        return tf.nn.max_pool(value=input_act_vars,
+                             ksize=(1,)+self.pool_size+(1,),
+                             strides=(1,)+self.strides+(1,),
+                             padding=self.padding_mode)
+
     def _get_mxts_increments_for_inputs(self):
-        if (self.maxpool_deeplift_mode==
-            MaxPoolDeepLiftMode.gradient):
-            return (self.
-                    _get_input_grad_given_outgrad(out_grad=self.get_mxts()))
-        elif (self.maxpool_deeplift_mode==
-              MaxPoolDeepLiftMode.scaled_gradient):
-            grad_times_diff_def = self._get_input_grad_given_outgrad(
-                   out_grad=self.get_mxts()*self._get_diff_from_reference_vars()) 
-            pcd_input_diff_default = (pseudocount_near_zero(
-                                     self._get_input_diff_from_reference_vars()))
-            return grad_times_diff_def/pcd_input_diff_default
+        if (self.maxpool_deeplift_mode==MaxPoolDeepLiftMode.gradient):
+            return tf.nn.gen_nn_ops._max_pool_grad(
+                orig_input=self._get_input_activation_vars(),
+                orig_output=self.get_activation_vars(),
+                grad=self.get_mxts(),
+                ksize=(1,)+self.pool_size+(1,),
+                strides=(1,)+self.strides+(1,),
+                padding=self.padding_mode) 
         else:
             raise RuntimeError("Unsupported maxpool_deeplift_mode: "+
                                str(self.maxpool_deeplift_mode))
@@ -239,23 +209,35 @@ class MaxPool2D(Pool2D):
 class AvgPool2D(Pool2D):
 
     def __init__(self, **kwargs):
-        super(AvgPool2D, self).__init__(pool_mode=B.PoolMode.avg, **kwargs) 
+        super(AvgPool2D, self).__init__(**kwargs) 
+
+    def _build_activation_vars(self, input_act_vars):
+        return tf.nn.avg_pool(value=input_act_vars,
+                             ksize=(1,)+self.pool_size+(1,),
+                             strides=(1,)+self.strides+(1,),
+                             padding=self.padding_mode)
 
     def _get_mxts_increments_for_inputs(self):
-        return super(AvgPool2D, self)._get_input_grad_given_outgrad(
-                                       out_grad=self.get_mxts())
+        return tf.nn.gen_nn_ops._avg_pool_grad(
+            orig_input_shape=self._get_input_activation_vars().get_shape(),
+            grad=self.get_mxts(),
+            ksize=(1,)+self.pool_size+(1,),
+            strides=(1,)+self.strides+(1,),
+            padding=self.padding_mode) 
 
 
 class Flatten(SingleInputMixin, OneDimOutputMixin, Node):
     
     def _build_activation_vars(self, input_act_vars):
-        return B.flatten_keeping_first(input_act_vars)
+        return tf.reshape(input_act_vars,
+                tf.pack([input_act_vars.get_shape()[0],
+                         tf.reduce_prod(input_act_vars.get_shape()[1:])
+                        ]))
 
     def _compute_shape(self, input_shape):
         return (None, np.prod(input_shape[1:]))
 
     def _get_mxts_increments_for_inputs(self):
         input_act_vars = self._get_input_activation_vars() 
-        return B.unflatten_keeping_first(
-                x=self.get_mxts(), like=input_act_vars
-            )
+        return tf.reshape(tensor=self.get_mxts(),
+                          shape=input_act_vars.get_shape())
