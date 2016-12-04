0c1d6bc6d50b4d714b6ea42fa0ef0b51f1fd630b
Avanti Shrikumar <avanti.shrikumar@gmail.com>
2016-12-04 14:51:15 -0800

    fprop for the convolutional model behaving
    Former-commit-id: ce9277a189559315fba29a18939fbd6bf8e523e0 [formerly bbefa71065558c59435d15423b551f78ab86c0a7]
    Former-commit-id: d5f861b74724ce6abf4add5fcf7c97aba132ca98

--- tests/conversion/test_convolutional_model.py
+++ tests/conversion/test_convolutional_model.py
@@ -9,7 +9,8 @@ import numpy as np
 from deeplift.conversion import keras_conversion as kc
 import deeplift.blobs as blobs
 from deeplift.blobs import NonlinearMxtsMode
-import theano
+from deeplift.util import compile_func
+import tensorflow as tf
 import keras
 from keras import models
 from keras import backend as K
@@ -19,41 +20,42 @@ class TestConvolutionalModel(unittest.TestCase):
 
     def setUp(self):
          
-        self.inp = np.random.randn(10*10*51*51).reshape(10,10,51,51)
+        self.inp = (np.random.randn(10*10*51*51)
+                    .reshape(10,10,51,51)).transpose(0,2,3,1)
         self.keras_model = keras.models.Sequential()
         conv_layer = keras.layers.convolutional.Convolution2D(
                         nb_filter=2, nb_row=4, nb_col=4, subsample=(2,2),
-                        activation="relu", input_shape=(10,51,51))
+                        activation="relu", input_shape=(51,51,10))
         self.keras_model.add(conv_layer)
         #for 1.0, Pooling2D is moved to layers.pooling
-        self.keras_model.add(keras.layers.convolutional.MaxPooling2D(
+        self.keras_model.add(keras.layers.pooling.MaxPooling2D(
                              pool_size=(4,4), strides=(2,2))) 
         self.keras_model.add(keras.layers.Flatten())
         self.keras_model.add(keras.layers.Dense(output_dim=1))
         self.keras_model.add(keras.layers.core.Activation("sigmoid"))
         self.keras_model.compile(loss="mse", optimizer="sgd")
-        self.keras_output_fprop_func = theano.function(
-                        [self.keras_model.layers[0].input],
-                        self.keras_model.layers[-1].get_output(train=False),
-                        allow_input_downcast=True)
+        self.keras_output_fprop_func = compile_func(
+                        [self.keras_model.layers[0].input,
+                         K.learning_phase()],
+                        self.keras_model.layers[-1].output)
 
-        grad = theano.grad(theano.tensor.sum(
-                   self.keras_model.layers[-2].get_output(train=False)[:,0]),
-                   self.keras_model.layers[0].input)
-        self.grad_func = theano.function(
-                         [self.keras_model.layers[0].input],
-                         grad, allow_input_downcast=True)
+        grad = tf.gradients(tf.reduce_sum(
+            self.keras_model.layers[-2].output[:,0]),
+            [self.keras_model.layers[0].input])
+        self.grad_func = compile_func(
+            [self.keras_model.layers[0].input,
+             K.learning_phase()], grad)
          
 
     def test_convert_model_fprop(self): 
         deeplift_model = kc.convert_sequential_model(model=self.keras_model)
-        deeplift_fprop_func = theano.function(
+        deeplift_fprop_func = compile_func(
                     [deeplift_model.get_layers()[0].get_activation_vars()],
-                    deeplift_model.get_layers()[-1].get_activation_vars(),
-                    allow_input_downcast=True)
-        np.testing.assert_almost_equal(deeplift_fprop_func(self.inp),
-                                   self.keras_output_fprop_func(self.inp),
-                                   decimal=6)
+                    deeplift_model.get_layers()[-1].get_activation_vars())
+        np.testing.assert_almost_equal(
+            deeplift_fprop_func(self.inp, False),
+            self.keras_output_fprop_func(self.inp),
+            decimal=6)
          
 
     def test_convert_model_backprop(self): 
