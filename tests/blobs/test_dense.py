183611aeb37e848d708a53bd9fb9b3c4273a3a35
Avanti Shrikumar <avanti.shrikumar@gmail.com>
2016-12-09 18:22:25 -0800

    added tests that run through the different multiplier modes
    Former-commit-id: 1193ac20abd87fa7fd88a75d224307080e806a0b [formerly 6b733e01299fd0d9754b81fc495c5fd1f0315817]
    Former-commit-id: 86befea89730e3569d0516e19fc17050f487d7b6

--- tests/blobs/test_dense.py
+++ tests/blobs/test_dense.py
@@ -56,4 +56,21 @@ class TestDense(unittest.TestCase):
         self.assertListEqual([list(x) for x in func(self.inp,
                               np.zeros_like(self.inp))],
                              [self.w2, self.w2])
-        
+ 
+    def test_running_of_different_dense_modes(self):
+        for mode in DenseMxtsMode.vals:
+            input_layer = blobs.Input(num_dims=None, shape=(None,4))
+            W = np.array([self.w1, self.w2]).T
+            b = np.array([-1.0, 1.0])
+            dense_layer = blobs.Dense(W=W, b=b,
+                                      dense_mxts_mode=mode)
+            dense_layer.set_inputs(input_layer)
+            dense_layer.build_fwd_pass_vars()
+            dense_layer.set_scoring_mode(blobs.ScoringMode.OneAndZeros)
+            dense_layer.set_active()
+            input_layer.update_mxts()
+            func = compile_func([input_layer.get_activation_vars(),
+                                 input_layer.get_reference_vars()],
+                                dense_layer.get_mxts())
+            dense_layer.update_task_index(task_index=0)
+            func(self.inp, np.zeros_like(self.inp))
