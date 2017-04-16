afa979ddcab29e7c56da31d477cc8b41f43e656b
AvantiShri <avanti.shrikumar@gmail.com>
2017-04-16 13:32:23 -0700

    added conv tests with batch norm

--- tests/blobs/test_deeplift_genomics_default_mode.py
+++ tests/blobs/test_deeplift_genomics_default_mode.py
@@ -57,11 +57,72 @@ class TestDense(unittest.TestCase):
                                   conv_mxts_mode=ConvMxtsMode.Linear,
                                   stride=1,
                                   border_mode=PaddingMode.valid,
-                                  channels_come_last=False)
+                                  channels_come_last=True)
         conv_layer.set_inputs(input_layer)
         relu_after_conv = blobs.ReLU(nonlinear_mxts_mode=
                                     NonlinearMxtsMode.DeepLIFT_GenomicsDefault) 
         relu_after_conv.set_inputs(conv_layer)
         relu_after_conv.build_fwd_pass_vars()
         self.assertEqual(relu_after_conv.nonlinear_mxts_mode,
+                         NonlinearMxtsMode.Rescale)
+
+    def test_relu_after_conv1d_batchnorm(self): 
+        input_layer = blobs.Input(num_dims=None,
+                                  shape=(None,2,2))
+        conv_layer = blobs.Conv1D(W=np.random.random((2,2,2)),
+                                  b=np.random.random((2,)),
+                                  conv_mxts_mode=ConvMxtsMode.Linear,
+                                  stride=1,
+                                  border_mode=PaddingMode.valid,
+                                  channels_come_last=True)
+        conv_layer.set_inputs(input_layer)
+        batch_norm = blobs.BatchNormalization(
+                        gamma=np.array([1.0, 1.0]), beta=np.array([-0.5, 0.5]),
+                        axis=-1, mean=np.array([-0.5, 0.5]),
+                        std=np.array([1.0, 1.0]), epsilon=0.001)
+        batch_norm.set_inputs(conv_layer)
+        relu_after_bn = blobs.ReLU(nonlinear_mxts_mode=
+                                    NonlinearMxtsMode.DeepLIFT_GenomicsDefault) 
+        relu_after_bn.set_inputs(batch_norm)
+        relu_after_bn.build_fwd_pass_vars()
+        self.assertEqual(relu_after_bn.nonlinear_mxts_mode,
+                         NonlinearMxtsMode.Rescale)
+
+    def test_relu_after_conv2d(self): 
+        input_layer = blobs.Input(num_dims=None,
+                                  shape=(None,2,2,2))
+        conv_layer = blobs.Conv2D(W=np.random.random((2,2,2,2)),
+                                  b=np.random.random((2,)),
+                                  conv_mxts_mode=ConvMxtsMode.Linear,
+                                  strides=(1,1),
+                                  border_mode=PaddingMode.valid,
+                                  channels_come_last=True)
+        conv_layer.set_inputs(input_layer)
+        relu_after_conv = blobs.ReLU(nonlinear_mxts_mode=
+                                    NonlinearMxtsMode.DeepLIFT_GenomicsDefault) 
+        relu_after_conv.set_inputs(conv_layer)
+        relu_after_conv.build_fwd_pass_vars()
+        self.assertEqual(relu_after_conv.nonlinear_mxts_mode,
+                         NonlinearMxtsMode.Rescale)
+
+    def test_relu_after_conv2d_batchnorm(self): 
+        input_layer = blobs.Input(num_dims=None,
+                                  shape=(None,2,2,2))
+        conv_layer = blobs.Conv2D(W=np.random.random((2,2,2,2)),
+                                  b=np.random.random((2,)),
+                                  conv_mxts_mode=ConvMxtsMode.Linear,
+                                  strides=(1,1),
+                                  border_mode=PaddingMode.valid,
+                                  channels_come_last=True)
+        conv_layer.set_inputs(input_layer)
+        batch_norm = blobs.BatchNormalization(
+                        gamma=np.array([1.0, 1.0]), beta=np.array([-0.5, 0.5]),
+                        axis=-1, mean=np.array([-0.5, 0.5]),
+                        std=np.array([1.0, 1.0]), epsilon=0.001)
+        batch_norm.set_inputs(conv_layer)
+        relu_after_bn = blobs.ReLU(nonlinear_mxts_mode=
+                                    NonlinearMxtsMode.DeepLIFT_GenomicsDefault) 
+        relu_after_bn.set_inputs(batch_norm)
+        relu_after_bn.build_fwd_pass_vars()
+        self.assertEqual(relu_after_bn.nonlinear_mxts_mode,
                          NonlinearMxtsMode.Rescale)
