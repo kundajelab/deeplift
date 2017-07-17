87289b2eab17b7f8afd0da17f61c7b3a4a1584dc
Avanti Shrikumar <avanti.shrikumar@gmail.com>
2017-07-17 12:02:57 -0700

    DeepLIFT_Genomics mode handles NoOp layers

--- tests/blobs/test_deeplift_genomics_default_mode.py
+++ tests/blobs/test_deeplift_genomics_default_mode.py
@@ -31,7 +31,7 @@ class TestDense(unittest.TestCase):
         self.assertEqual(relu_after_dense.nonlinear_mxts_mode,
                          NonlinearMxtsMode.RevealCancel)
 
-    def test_relu_after_dense_batchnorm(self): 
+    def test_relu_after_dense_batchnorm_noop_noop(self): 
         input_layer = blobs.Input(num_dims=None,
                                   shape=(None,4))
         dense_layer = blobs.Dense(W=np.random.random((4,2)),
@@ -43,9 +43,13 @@ class TestDense(unittest.TestCase):
                         axis=-1, mean=np.array([-0.5, 0.5]),
                         std=np.array([1.0, 1.0]), epsilon=0.001)
         batch_norm.set_inputs(dense_layer)
+        noop_layer1 = blobs.NoOp()
+        noop_layer1.set_inputs(batch_norm)
+        noop_layer2 = blobs.NoOp()
+        noop_layer2.set_inputs(noop_layer1)
         relu_after_bn = blobs.ReLU(nonlinear_mxts_mode=
                                    NonlinearMxtsMode.DeepLIFT_GenomicsDefault) 
-        relu_after_bn.set_inputs(batch_norm)
+        relu_after_bn.set_inputs(noop_layer2)
         relu_after_bn.build_fwd_pass_vars()
         self.assertEqual(relu_after_bn.nonlinear_mxts_mode,
                          NonlinearMxtsMode.RevealCancel)
