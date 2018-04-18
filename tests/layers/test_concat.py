51ea4ac8fdf2f34174162708ac3d4b1a4a89e1ae
Avanti Shrikumar <avanti.shrikumar@gmail.com>
2018-04-17 22:39:26 -0700

    test for concat in

--- tests/layers/test_concat.py
+++ tests/layers/test_concat.py
@@ -6,28 +6,29 @@ from unittest import skip
 import sys
 import os
 import numpy as np
-import deeplift.blobs as blobs
-from deeplift.blobs import DenseMxtsMode
+import deeplift.layers as layers
+from deeplift.layers import DenseMxtsMode
 from deeplift.util import compile_func
 
 
 class TestConcat(unittest.TestCase):
 
     def setUp(self):
-        self.input_layer1 = blobs.Input(num_dims=None, shape=(None,1,1,1))
-        self.input_layer2 = blobs.Input(num_dims=None, shape=(None,1,1,1))
-        self.concat_layer = blobs.Concat(axis=1)
+        self.input_layer1 = layers.Input(batch_shape=(None,1,1,1))
+        self.input_layer2 = layers.Input(batch_shape=(None,1,1,1))
+        self.concat_layer = layers.Concat(axis=1)
         self.concat_layer.set_inputs([self.input_layer1, self.input_layer2])
-        self.flatten_layer = blobs.Flatten()
+        self.flatten_layer = layers.Flatten()
         self.flatten_layer.set_inputs(self.concat_layer)
-        self.dense_layer = blobs.Dense(
-         W=np.array([([1,2])]).T, b=[1], dense_mxts_mode=DenseMxtsMode.Linear)
+        self.dense_layer = layers.Dense(
+         kernel=np.array([([1,2])]).T,
+         bias=[1], dense_mxts_mode=DenseMxtsMode.Linear)
         self.dense_layer.set_inputs(self.flatten_layer)
         self.dense_layer.build_fwd_pass_vars()
 
         self.input_layer1.reset_mxts_updated()
         self.input_layer2.reset_mxts_updated()
-        self.dense_layer.set_scoring_mode(blobs.ScoringMode.OneAndZeros)
+        self.dense_layer.set_scoring_mode(layers.ScoringMode.OneAndZeros)
         self.dense_layer.set_active()
         self.input_layer1.update_mxts()
         self.input_layer2.update_mxts()
@@ -39,19 +40,24 @@ class TestConcat(unittest.TestCase):
         func = compile_func([self.input_layer1.get_activation_vars(),
                                 self.input_layer2.get_activation_vars()],
                                 self.concat_layer.get_activation_vars())
-        np.testing.assert_allclose(func(self.inp1, self.inp2),
+        np.testing.assert_allclose(func([self.inp1, self.inp2]),
                                    np.array([[[[1]],[[1]]],[[[2]],[[2]]]]))
 
     def test_concat_backprop(self):
         func = compile_func([
                 self.input_layer1.get_activation_vars(),
-                self.input_layer2.get_activation_vars()],
-                #self.concat_layer.get_mxts(),
+                self.input_layer2.get_activation_vars(),
+                self.input_layer1.get_reference_vars(),
+                self.input_layer2.get_reference_vars()],
                 [self.input_layer1.get_mxts(),
-                 self.input_layer2.get_mxts()],
+                 self.input_layer2.get_mxts()]
                 )
-        print(func(self.inp1, self.inp2))
+        print(func([self.inp1, self.inp2,
+                    np.zeros_like(self.inp1), np.zeros_like(self.inp2)]))
         self.dense_layer.update_task_index(task_index=0)
-        np.testing.assert_allclose(func(self.inp1, self.inp2),
+        np.testing.assert_allclose(func([self.inp1, self.inp2,
+                                         np.zeros_like(self.inp1),
+                                         np.zeros_like(self.inp2)]),
                                    [np.array([[[[1]]],[[[1]]]]),
                                     np.array([[[[2]]],[[[2]]]])])
+
