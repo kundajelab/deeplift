cf494e006ef0f58ddf4efcc135165e74bb6c4e4b
Avanti Shrikumar <avanti.shrikumar@gmail.com>
2018-04-19 20:57:25 -0700

    updated pooling test

--- tests/layers/test_pool2d.py
+++ tests/layers/test_pool2d.py
@@ -6,10 +6,10 @@ from unittest import skip
 import sys
 import os
 import numpy as np
-import deeplift.blobs as blobs
-from deeplift.blobs import DenseMxtsMode
-from deeplift.blobs import MaxPoolDeepLiftMode
-from deeplift.blobs.convolution import PaddingMode
+import deeplift.layers as layers
+from deeplift.layers import DenseMxtsMode
+from deeplift.layers import MaxPoolDeepLiftMode
+from deeplift.layers.convolutional import PaddingMode
 from deeplift.util import compile_func
 import itertools
 
@@ -46,37 +46,35 @@ class TestPool(unittest.TestCase):
                                    [1,2,5,1],
                                    [8,7,6,1],
                                    [7,1,9,10]]]]).transpose(0,2,3,1)
-        self.input_layer = blobs.Input(
-                            num_dims=None,
-                            shape=(None,4,4,2))
+        self.input_layer = layers.Input(batch_shape=(None,4,4,2))
 
     def create_small_net_with_pool_layer(self, pool_layer,
                                                outputs_per_channel):
         self.pool_layer = pool_layer
         self.pool_layer.set_inputs(self.input_layer)
 
-        self.flatten_layer = blobs.Flatten()
+        self.flatten_layer = layers.Flatten()
         self.flatten_layer.set_inputs(self.pool_layer)
 
-        self.dense_layer = blobs.Dense(
-                           W=(np.array([
+        self.dense_layer = layers.Dense(
+                           kernel=(np.array([
                             list(itertools.chain(*[[2,3] for i
                              in range(outputs_per_channel)]))]
                             )).astype("float32").T,
-                           b=np.array([1]).astype("float32"),
+                           bias=np.array([1]).astype("float32"),
                            dense_mxts_mode=DenseMxtsMode.Linear)
         self.dense_layer.set_inputs(self.flatten_layer)
 
         self.dense_layer.build_fwd_pass_vars()
-        self.dense_layer.set_scoring_mode(blobs.ScoringMode.OneAndZeros)
+        self.dense_layer.set_scoring_mode(layers.ScoringMode.OneAndZeros)
         self.dense_layer.set_active()
         self.input_layer.update_mxts()
         
     def test_fprop_maxpool2d(self): 
 
-        pool_layer = blobs.MaxPool2D(pool_size=(2,2),
+        pool_layer = layers.MaxPool2D(pool_size=(2,2),
                           strides=(1,1),
-                          padding_mode=PaddingMode.valid,
+                          padding=PaddingMode.valid,
                           maxpool_deeplift_mode=MaxPoolDeepLiftMode.gradient)
         self.create_small_net_with_pool_layer(pool_layer,
                                               outputs_per_channel=9)
@@ -102,14 +100,14 @@ class TestPool(unittest.TestCase):
 
     def test_fprop_avgpool2d(self): 
 
-        pool_layer = blobs.AvgPool2D(pool_size=(2,2),
+        pool_layer = layers.AvgPool2D(pool_size=(2,2),
                                   strides=(1,1),
-                                  padding_mode=PaddingMode.valid)
+                                  padding=PaddingMode.valid)
         self.create_small_net_with_pool_layer(pool_layer,
                                               outputs_per_channel=9)
 
         func = compile_func([self.input_layer.get_activation_vars()],
-                           self.pool_layer.get_activation_vars())
+                             self.pool_layer.get_activation_vars())
         np.testing.assert_almost_equal(func([self.reference_inps[0],
                                              self.reference_inps[0]-1]),
                                        0.25*np.array(
@@ -127,9 +125,9 @@ class TestPool(unittest.TestCase):
                                           [11,16,19]]]]).transpose(0,2,3,1))
 
     def test_backprop_maxpool2d_gradients(self):
-        pool_layer = blobs.MaxPool2D(pool_size=(2,2),
+        pool_layer = layers.MaxPool2D(pool_size=(2,2),
                   strides=(1,1),
-                  padding_mode=PaddingMode.valid,
+                  padding=PaddingMode.valid,
                   maxpool_deeplift_mode=MaxPoolDeepLiftMode.gradient)
         self.create_small_net_with_pool_layer(pool_layer,
                                               outputs_per_channel=9)
@@ -140,8 +138,8 @@ class TestPool(unittest.TestCase):
                 self.input_layer.get_reference_vars()],
                                    self.input_layer.get_mxts())
         np.testing.assert_almost_equal(
-            func(self.backprop_test_inps,
-                 np.ones_like(self.backprop_test_inps)*self.reference_inps),
+            func([self.backprop_test_inps,
+                  np.ones_like(self.backprop_test_inps)*self.reference_inps]),
                                   np.array(
                                   [[np.array([[1, 0, 0, 0],
                                      [0, 0, 2, 0],
@@ -161,9 +159,9 @@ class TestPool(unittest.TestCase):
                                      [0, 0, 1, 1]])*3]]).transpose(0,2,3,1))
 
     def test_backprop_avgpool2d(self):
-        pool_layer = blobs.AvgPool2D(pool_size=(2,2),
+        pool_layer = layers.AvgPool2D(pool_size=(2,2),
                   strides=(1,1),
-                  padding_mode=PaddingMode.valid)
+                  padding=PaddingMode.valid)
         self.create_small_net_with_pool_layer(pool_layer,
                                               outputs_per_channel=9)
 
@@ -175,9 +173,9 @@ class TestPool(unittest.TestCase):
                                    [2, 4, 4, 2],
                                    [2, 4, 4, 2],
                                    [1, 2, 2, 1]]).astype("float32") 
-        np.testing.assert_almost_equal(func(
+        np.testing.assert_almost_equal(func([
                   self.backprop_test_inps,
-                  np.ones_like(self.backprop_test_inps)*self.reference_inps),
+                  np.ones_like(self.backprop_test_inps)*self.reference_inps]),
                               np.array(
                               [[avg_pool_grads*2*0.25,
                                 avg_pool_grads*3*0.25], 
