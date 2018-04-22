25b54d0a46066f5085fd9d9473ecd350f224e845
Avanti Shrikumar <avanti.shrikumar@gmail.com>
2018-04-21 18:19:53 -0700

    test for conv1d sequential model works yay

--- tests/conversion/test_conv1d_model.py
+++ tests/conversion/test_conv1d_model.py
@@ -7,8 +7,8 @@ import sys
 import os
 import numpy as np
 from deeplift.conversion import kerasapi_conversion as kc
-import deeplift.blobs as blobs
-from deeplift.blobs import NonlinearMxtsMode
+import deeplift.layers as layers
+from deeplift.layers import NonlinearMxtsMode
 from deeplift.util import compile_func
 import tensorflow as tf
 import keras
@@ -45,22 +45,27 @@ class TestConvolutionalModel(unittest.TestCase):
             [self.keras_model.layers[0].input])[0]
         self.grad_func = compile_func(
             [self.keras_model.layers[0].input,
-             K.learning_phase()], grad)
-         
+             K.learning_phase()], grad) 
+        self.saved_file_path = "convmodel.h5"
+        if (os.path.isfile(self.saved_file_path)):
+            os.remove(self.saved_file_path)
+        self.keras_model.save(self.saved_file_path)
 
     def test_convert_conv1d_model_forward_prop(self): 
-        deeplift_model = kc.convert_sequential_model(model=self.keras_model)
+        deeplift_model =\
+            kc.convert_model_from_saved_files(self.saved_file_path) 
         deeplift_fprop_func = compile_func(
-                    [deeplift_model.get_layers()[0].get_activation_vars()],
-                     deeplift_model.get_layers()[-1].get_activation_vars())
+                inputs=[deeplift_model.get_layers()[0].get_activation_vars()],
+                outputs=deeplift_model.get_layers()[-1].get_activation_vars())
         np.testing.assert_almost_equal(
             deeplift_fprop_func(self.inp),
-            self.keras_output_fprop_func(self.inp),
+            self.keras_output_fprop_func([self.inp, 0]),
             decimal=6)
          
 
     def test_convert_conv1d_model_compute_scores(self): 
-        deeplift_model = kc.convert_sequential_model(model=self.keras_model)
+        deeplift_model =\
+            kc.convert_model_from_saved_files(self.saved_file_path) 
         deeplift_contribs_func = deeplift_model.\
                                      get_target_contribs_func(
                                       find_scores_layer_idx=0,
@@ -71,4 +76,9 @@ class TestConvolutionalModel(unittest.TestCase):
                                       batch_size=10,
                                       progress_update=None),
             #when biases are 0 and ref is 0, deeplift is the same as grad*inp 
-            self.grad_func(self.inp, False)*self.inp, decimal=6)
+            self.grad_func([self.inp, 0])*self.inp, decimal=6)
+
+    def tearDown(self):
+        if (os.path.isfile(self.saved_file_path)):
+            os.remove(self.saved_file_path)
+        
