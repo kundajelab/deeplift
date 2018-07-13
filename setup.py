0b2f60bd5039c7387ee553057fa104e9231a7969
Avanti Shrikumar <avanti.shrikumar@gmail.com>
2018-07-13 22:46:22 +0200

    updated setup to handle tensorflow gpu

--- setup.py
+++ setup.py
@@ -3,13 +3,15 @@ from distutils.core import setup
 if __name__== '__main__':
     setup(include_package_data=True,
           description='DeepLIFT (Deep Learning Important FeaTures)',
-          url='NA',
-          download_url='NA',
+          url='https://github.com/kundajelab/deeplift',
           version='0.6.5',
           packages=['deeplift',
                     'deeplift.layers', 'deeplift.visualization',
                     'deeplift.conversion'],
           setup_requires=[],
-          install_requires=['numpy>=1.9', 'tensorflow>=1.7'],
+          install_requires=['numpy>=1.9'],
+          extras_require={
+            'tensorflow': ['tensorflow>=1.7'],
+            'tensorflow with gpu': ['tensorflow-gpu>=1.7']},
           scripts=[],
           name='deeplift')
